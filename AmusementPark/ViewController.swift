9d0b1dabcad670552bd8b9cbbbc956f247826d83
Jason Vest <jason@jasonvest.com>
2019-07-22 15:22:21 -0700

    Initial commit

---
+++ AmusementPark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmusementPark
+//
+//  Created by Jason Vest on 7/22/19.
+//  Copyright © 2019 Jason Vest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
